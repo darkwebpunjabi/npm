ccf0dff7b0d78c29827ed9be22ade0ed76a0c976
darkwebpunjabi <62046429+darkwebpunjabi@users.noreply.github.com>
2020-03-13 23:03:59 -0700

    ChP02

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Starting app.js');
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+var res = notes.addNote();
+console.log(res);
+//var user = os.userInfo();;
+/*fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err) {
+    if (err) {
+        console.log('Unable to write to file')
+    }
+});*/
+//console.log(user);
